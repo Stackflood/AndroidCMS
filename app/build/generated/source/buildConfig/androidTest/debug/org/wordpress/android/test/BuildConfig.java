2ec6e2cb9f2e6a5c44f80dee4e45d6df336148b3
Stackflood <rahul.nsit05@gmail.com>
2015-12-14 22:49:20 -0800

    Comments section completeed

---
+++ app/build/generated/source/buildConfig/androidTest/debug/org/wordpress/android/test/BuildConfig.java
@@ -0,0 +1,27 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.wordpress.android.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.wordpress.android.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 175;
+  public static final String VERSION_NAME = "3.9";
+  // Fields from build type: debug
+  public static final String APP_PN_KEY = "org.wordpress.android.debug.build";
+  public static final String DB_SECRET = "wordpress";
+  public static final String GCM_ID = "androidcms-1065";
+  public static final String HELPSHIFT_API_DOMAIN = "wordpress.org";
+  public static final String HELPSHIFT_API_ID = "wordpress";
+  public static final String HELPSHIFT_API_KEY = "wordpress";
+  public static final String MIXPANEL_TOKEN = "wordpress";
+  public static final String OAUTH_APP_ID = "40700";
+  public static final String OAUTH_APP_SECRET = "3vKgpCVLXmVzW4XZgbQHNI9DyH3UolN2HHcwUIjv9PEKhs4GbrdrgHZK1Y7Nc61p";
+  public static final String OAUTH_REDIRECT_URI = "http://android.wordpress.org/";
+  public static final String SIMPERIUM_APP_NAME = "wordpress";
+  public static final String SIMPERIUM_APP_SECRET = "wordpress";
+  // Fields from default config.
+}
